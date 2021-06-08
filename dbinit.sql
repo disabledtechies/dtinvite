1bc133702d2b1ed97f9a77a791836c2b12323171
sarahemm <github@sen.cx>
2021-06-08 14:16:17 +0000

    Moved definition from footnote to top.

--- dbinit.sql
+++ dbinit.sql
@@ -1,5 +1,5 @@
 BEGIN TRANSACTION;
-INSERT INTO reasons (shortdesc, fulldesc) VALUES ("Disabled", 'I identify as disabled (* if unsure, see below for our approach to defining "disability")');
+INSERT INTO reasons (shortdesc, fulldesc) VALUES ("Disabled", 'I identify as disabled.');
 INSERT INTO reasons (shortdesc, fulldesc) VALUES ("Has disabling condition", "I experience one or more disabling conditions, though I don't use the word ""disabled"" to describe myself.");
 INSERT INTO reasons (shortdesc, fulldesc) VALUES ("Questioning", "I don't know/I'm questioning whether I might have a disability (e.g. There's something I'm trying to figure out about myself, I'm not getting good answers for it yet, and I believe that I may get better suggestions or ideas from disabled people)");
 INSERT INTO reasons (shortdesc, fulldesc) VALUES ("Not disabled", 'I am not disabled in any way (e.g. I am a non-disabled ally; I am a non-disabled recruiter hoping to connect with the community, etc.)');
